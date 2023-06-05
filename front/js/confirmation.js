ab4b3d1705967569fe70f1f008c3d4b67a63a4f5
Alex Comp <alexandrecompin@gmail.com>
2023-06-05 22:29:15 +0200

    ajout de l'id dans le message de validation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,9 +1,9 @@
 // récupération id (numéro de commande)
-const idSearch = new URLSearchParams(url)
-const idGet = idSearch.get('id')
+const idUrl = new URL(window.location.href);
+const idGet = idUrl.searchParams.get("id");
 
 // ajout du numéro de commande
-const orderNumber = document.getElementById('orderId')
-orderNumber.innerText = `${idGet}`
+const orderNumber = document.getElementById("orderId");
+orderNumber.innerText = `${idGet}`;
 
-localStorage.clear()
+localStorage.clear();
--- front/js/product.js
+++ front/js/product.js
@@ -1,94 +1,89 @@
 // récupération de l'id du produit
 const newUrl = new URL(window.location.href);
-let idProduct = newUrl.searchParams.get('id');
+let idProduct = newUrl.searchParams.get("id");
 
 //récupération des données du produit spécifique via l'API
 fetch(`http://localhost:3000/api/products/` + idProduct)
   .then(function (response) {
-      return response.json()
-    })
+    return response.json();
+  })
   .then(function (data) {
-      displayProduct(data);
-    })
-
+    displayProduct(data);
+  });
 
 function displayProduct(data) {
-
-  const img = document.querySelector('.item__img img');
+  const img = document.querySelector(".item__img img");
   img.src = data.imageUrl;
   img.alt = data.altTxt;
 
-  const title = document.querySelector('#title');
-  title.innerText = data.name; 
+  const title = document.querySelector("#title");
+  title.innerText = data.name;
 
-  const prix = document.getElementById('price');
+  const prix = document.getElementById("price");
   prix.innerText = data.price;
 
-  const description = document.getElementById('description');
+  const description = document.getElementById("description");
   description.innerText = data.description;
 
-  const colors = document.getElementById('colors');
+  const colors = document.getElementById("colors");
   const choiceColor = data.colors;
 
   // Boucle le choix des couleurs disponible
   for (let i = 0; i < choiceColor.length; i++) {
-    
-    const colorOption = document.createElement('option');
-    colorOption.setAttribute('value', choiceColor[i]);
-    
+    const colorOption = document.createElement("option");
+    colorOption.setAttribute("value", choiceColor[i]);
+
     colors.appendChild(colorOption);
-    colorOption.innerText = choiceColor[i]
+    colorOption.innerText = choiceColor[i];
   }
 }
 
-
 // Gestion du panier
 
-const btnAddToCart= document.querySelector("#addToCart") 
-btnAddToCart.addEventListener("click", addToCart)
-
+const btnAddToCart = document.querySelector("#addToCart");
+btnAddToCart.addEventListener("click", addToCart);
 
 function addToCart() {
-
-  let choiceQuantity = document.querySelector('#quantity').value;
-  let choiceColor = document.querySelector('option:checked').value;
+  let choiceQuantity = document.querySelector("#quantity").value;
+  let choiceColor = document.querySelector("option:checked").value;
 
   let item = {
-    id : idProduct,
-    quantity : Number(choiceQuantity),
-    color : choiceColor
-  }
+    id: idProduct,
+    quantity: Number(choiceQuantity),
+    color: choiceColor,
+  };
 
-  if (choiceQuantity == 0 && choiceColor == '') {
-    alert('Veuillez selectioner une couleur/quantitée')
-    return false
+  if (choiceQuantity == 0 && choiceColor == "") {
+    alert("Veuillez selectioner une couleur/quantitée");
+    return false;
   }
 
   // si le localstorage est vide
-  if (localStorage.cart == null) { 
-    const cart = []
-    localStorage.setItem('cart', cart)
-    cart.push(item)
-    localStorage.setItem('cart', JSON.stringify(cart))
+  if (localStorage.cart == null) {
+    const cart = [];
+    localStorage.setItem("cart", cart);
+    cart.push(item);
+    localStorage.setItem("cart", JSON.stringify(cart));
 
-  // si le localstorage contient des datas
+    // si le localstorage contient des datas
   } else {
-
     // on récupère dans un tableau le panier du local
-    const cart = JSON.parse(localStorage.getItem('cart'));
+    const cart = JSON.parse(localStorage.getItem("cart"));
 
     // cherche dans le tableau même id et même couleur, si rien return undefined
-    let foundItem = cart.find(item => item.id === idProduct && item.color === choiceColor);
-    
+    let foundItem = cart.find(
+      (item) => item.id === idProduct && item.color === choiceColor
+    );
+
     // si différent de undefined, c'est qui existe déja, donc quantité += avec origine
     if (foundItem != undefined) {
       foundItem.quantity += Number(choiceQuantity);
-      localStorage.setItem('cart', JSON.stringify(cart))
+      localStorage.setItem("cart", JSON.stringify(cart));
 
-    // sinon quantity choix utilisateur, on le push dans le local
+      // sinon quantity choix utilisateur, on le push dans le local
     } else {
       cart.push(item);
-      localStorage.setItem('cart', JSON.stringify(cart))
+      localStorage.setItem("cart", JSON.stringify(cart));
     }
-  } 
+  }
 }
